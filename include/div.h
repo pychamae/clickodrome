3c4dfa3fc5d70a060125bfbdc8049a93fb8cfab8
Firekiking <kgalaxie84@gmail.com>
2024-04-22 14:22:13 +0200

    Ajout des headers div et gui

---
+++ include/div.h
@@ -0,0 +1,56 @@
+#ifndef		__DIV_H__
+
+#define		__DIV_H__
+
+#include	"vector.h"
+
+#include	<lapin.h>
+
+typedef struct		s_div
+{
+  const char		*name;
+  t_bunny_position	pos;  // pos top left
+  t_bunny_size		size;
+  t_vector		labels;
+  t_vector		buttons;
+  t_vector		text_boxes;
+  t_vector		pictures;
+  t_vector		timer;
+}			t_div;
+
+t_div			*efnew_div(const char			*name,
+				   t_bunny_position		pos,
+				   t_bunny_size			size);
+void			efdelete_div(t_div			*div);
+void			efdisplay_div(t_div			*div,
+				      t_bunny_pixelarray	*px);
+void			efadd_button(t_div			*div,
+				     const char			*name,
+				     t_bunny_position		pos,
+				     t_bunny_size		size,
+				     const char			*text,
+				     t_bunny_color		*font_color,
+				     t_bunny_color		*bg);
+void			efadd_label(t_div			*div,
+				    t_bunny_position		pos,
+				    const char			*name,
+				    t_bunny_size		size,
+				    const char			*text,
+				    t_bunny_color		*font_color,
+				    t_bunny_color		*bg);
+void			efadd_text_boxes(t_div			*div,
+					 const char		*name,
+					 t_bunny_position	pos,
+					 t_bunny_size		size,
+					 const char		*text,
+					 t_bunny_color		*font_color,
+					 t_bunny_color		*bg);
+void			efadd_picture(t_div			*div,
+				      const char		*name,
+				      t_bunny_position		pos,
+				      t_bunny_size		size,
+				      const char		*filename);
+void			efadd_timer(t_div			*div,
+				   const char			*name);
+
+#endif //	__DIV_H__
--- include/gui.h
+++ include/gui.h
@@ -2,9 +2,27 @@
 
 #define			__GUI_H__
 
+#include		"vector.h"
+#include		"div.h"
+
+#include		<lapin.h>
+
 typedef struct		s_gui
 {
-  t_vector		
+  const char		*conf_file;
+  t_vector		components;
+  t_vector		divs;
 }			t_gui;
+
+t_gui			*efnew_gui(const char			*file);
+void			efdelete_gui(t_gui			*gui);
+void			efload_conf_gui(t_gui			*gui);
+void			efinteract_gui(t_gui			*ck,
+				       const char		*type,
+				       const char		*div_name,
+				       const char		*obj_name,
+				       ...);
+void			efdisplay_gui(t_gui			*ck,
+				      t_bunny_pixelarray	*px);
 
 #endif //		__GUI_H__
