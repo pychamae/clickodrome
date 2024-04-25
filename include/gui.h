0fca71e226d2ec14e009e743ebc1cb3438f69f76
romain.cescutti <romain.cescutti@gagarine.efrits.fr>
2024-04-25 11:34:46 +0200

    added draw.h to gui.h

--- include/gui.h
+++ include/gui.h
@@ -4,6 +4,7 @@
 
 #include		"vector.h"
 #include		"div.h"
+#include                "draw.h"
 
 #include		<lapin.h>
 
@@ -46,16 +47,16 @@ void			efdisplay_gui(t_gui			*ck,
 				      t_bunny_pixelarray	*px);
 void			efadd_button_gui(t_gui			*gui,
 					 t_div                  *divname,
-					 const char			*name,
-					 t_bunny_position		pos,
+					 const char		*name,
+					 t_bunny_position	pos,
 					 t_bunny_size		size,
-					 const char			*text,
+					 const char		*text,
 					 t_bunny_color		*font_color,
 					 t_bunny_color		*hover_color,
 					 t_bunny_color		*bg,
-					 t_vector		        *functions);
+					 t_vector	        *functions);
 void			efadd_label_gui(t_gui			*gui,
-					t_div                  *divname,
+					t_div                   *divname,
 				    t_bunny_position		pos,
 				    const char			*name,
 				    t_bunny_size		size,
@@ -63,7 +64,7 @@ void			efadd_label_gui(t_gui			*gui,
 				    t_bunny_color		*font_color,
 				    t_bunny_color		*bg);
 void			efadd_text_box_gui(t_gui		*gui,
-					   t_div                  *divname,
+					   t_div                *divname,
 				       const char		*name,
 				       t_bunny_position		pos,
 				       t_bunny_size		size,
@@ -78,16 +79,16 @@ void			efadd_picture_gui(t_gui			*gui,
 				      t_bunny_size		size,
 				      const char		*filename);
 void			efadd_timer_gui(t_gui			*gui,
-					t_div                  *divname,
+					t_div                   *divname,
 				    const char			*name,
 				    t_vector			*functions);
-void                 efadd_button_cnf(t_bunny_configuration *cnf,
-			  t_gui *gui);
+void                  efadd_button_cnf(t_bunny_configuration    *cnf,
+				       t_gui                    *gui);
 void                      efadd_label_cnf(t_bunny_configuration *cnf,
-		     t_gui *gui);
+					  t_gui                 *gui);
 void                    efadd_picture_cnf(t_bunny_configuration *cnf,
-		       t_gui *gui);
+					  t_gui                 *gui);
 void                   efadd_text_box_cnf(t_bunny_configuration *cnf,
-			t_gui *gui);
+					  t_gui                 *gui);
 
 #endif //		__GUI_H__
